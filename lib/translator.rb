20e67126606e588144b01b1ce0ed30ac4a177db0
Sharon Watkins <swatkins876@gmail.com>
2019-08-13 16:02:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,9 +5,16 @@ def load_library
   emoticons = YAML.load_file(file_path)
 end
 
-def get_japanese_emoticon
- 
-end
+#def get_japanese_emoticon
+ def get_japanese_emoticon(file_path, emoticon)
+	  library = load_library(file_path)
+  if library["get_emoticon"][emoticon]
+    library["get_emoticon"][emoticon]
+  else
+    return "Sorry, that emoticon was not found"
+  end
+end	
+
 
 def get_english_meaning
   # code goes here
