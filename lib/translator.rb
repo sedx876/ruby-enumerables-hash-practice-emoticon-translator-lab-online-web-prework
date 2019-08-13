192e12195d81f00d1903d6f0c5922de417cce709
Sharon Watkins <swatkins876@gmail.com>
2019-08-13 16:00:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,17 +5,9 @@ def load_library
   emoticons = YAML.load_file(file_path)
 end
 
-#def get_japanese_emoticon
- def get_japanese_emoticon(path, emoji)
-  hash = load_library(path)
-  hash["get_emoticon"].each do |k,v|
-      if k == emoji
-        return v
-    end
-  end
-  return "Sorry, that emoticon was not found"
-end	end
-#end
+def get_japanese_emoticon
+ 
+end
 
 def get_english_meaning
   # code goes here
